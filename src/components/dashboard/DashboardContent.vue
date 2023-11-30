afe8e4daffaaf39f1270578d40e474aa8b7efdda
Decio Neto <deciom.neto10@gmail.com>
2023-11-30 10:32:31 -0300

    chore: separate dashboard components

---
+++ src/components/dashboard/DashboardContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-1 space-y-4 p-8 pt-6">
+    <DashboardHeader />
+
+    <Tabs default-value="overview" class="space-y-4">
+      <DashboardTabsList />
+
+      <DashboardTabsOverview />
+    </Tabs>
+  </div>
+</template>
---
+++ src/components/dashboard/tabs/DashboardTabsList.vue
@@ -0,0 +1,8 @@
+<template>
+  <TabsList>
+    <TabsTrigger value="overview"> Overview </TabsTrigger>
+    <TabsTrigger value="analytics" disabled> Analytics </TabsTrigger>
+    <TabsTrigger value="reports" disabled> Reports </TabsTrigger>
+    <TabsTrigger value="notifications" disabled> Notifications </TabsTrigger>
+  </TabsList>
+</template>
